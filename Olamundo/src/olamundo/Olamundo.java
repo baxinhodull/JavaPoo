5936fe6c304c6a2d3442a404c604da6dc708c588
Carlos Henrique Junior <93613216+baxinhodull@users.noreply.github.com>
2024-10-11 10:58:40 -0300

    Add files via upload

---
+++ Olamundo/src/olamundo/Olamundo.java
@@ -0,0 +1,15 @@
+
+package olamundo;
+
+/**
+ *
+ * @author Carlos_Jr
+ */
+public class Olamundo {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("Ola, mundo");
+    }
+    
+}
